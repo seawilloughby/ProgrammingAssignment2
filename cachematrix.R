e6503a6da6515bb58725e9b1391cacacdc333106
seawilloughby <clwilloughby@gmail.com>
2014-12-20 12:53:35 -0600

    This might be it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+    i <- NULL
+    set <- function(y) {
+        x <<-ls- y
+        i <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) i <<- solve
+    getsolve <- function() i
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
+## This function opens the matrix, stores it in cashe memory, and computes a mean
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {  #x is an object from makeVector
+    i <- x$getsolve() #when called, first gets mean of x (from makecvvector)
+    
+    #if is cached value, then...
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)   #and then returns the mean
+    }
+    
+    #if no cached data, gets the original fector (is x$getmean() is NULL)
+    data <- x$get()
+    # calculate mean
+    i <- solve(data, ...)
+    #set mean value back in object
+    x$setsolve(i)
+    #also return the mean, duh. 
+    i
 }
